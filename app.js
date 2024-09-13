ebee86556899476a9f335fe77ae1797fff0d0cd6
Arslan Ali <muhammadarslanrajput12@gmail.com>
2024-09-13 18:42:15 +0500

    Nested Example

---
+++ app.js
@@ -0,0 +1,14 @@
+
+let array = [[1, 2, 3, 4],[5, 6,7],[8,9],[10]];
+
+for (let i = 0; i < array.length; i++) {
+console.log("========== "+i+" ==========");
+    for (let j = 0; j < array.length; j++) {
+        if(array[i][j]!==undefined){console.log(array[i][j]);
+        }
+        
+    }
+}
+
+
+
